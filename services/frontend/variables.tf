a11f5c828e73322831c49ea7a102e5f6595e8506
Jake Coker <jake.coker91@gmail.com>
2023-12-06 14:47:59 -0800

    updated variable names

--- services/frontend/variables.tf
+++ services/frontend/variables.tf
@@ -9,12 +9,12 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "db_remote_state_bucket" {
+variable "remote_state_bucket" {
   description = "The name of the S3 bucket for the DB remote state"
   type        = string
 }
 
-variable "db_remote_state_key" {
+variable "remote_state_key" {
   description = "The path for the DB remote state in S3"
   type        = string
 }
